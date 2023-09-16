1710cf4dfbbfa506bd115870acec77a1f5118f0c
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-09-17 00:11:18 +0430

    API

--- LearningApp/LearningApp/settings.py
+++ LearningApp/LearningApp/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,9 @@ INTERNAL_IPS = [
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 minutes
 CACHE_MIDDLEWARE_KEY_PREFIX = 'LearningApp'
+
+REST_FRAMEWORK = {
+ 'DEFAULT_PERMISSION_CLASSES': [
+ 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+ ]
+}
